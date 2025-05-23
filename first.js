6acd96d233d29dbbc3e843599a1a7f719c86c298
Bhagya <patilbhagyashri0910@gmail.com>
2025-05-23 17:58:29 +0530

    First Commit

---
+++ first.js
@@ -0,0 +1,63 @@
+// let score = 20;
+// let grade;
+
+// if(score>=90 && score<=100){
+//    grade="A";
+// }
+// else if(score>=70 && score<=89){
+//     grade="B";
+// }
+// else if(score>=60 && score<=69){
+//     grade="C";
+// }
+// else if(score>=50 && score<=59){
+//     grade="D";
+// }
+// else{
+//     grade="F";
+// }
+
+// console.log("Your Grade is :", grade);
+
+// let fName = prompt("Enter full name");
+// console.log("@" + fName + fName.length);
+
+// let arr = [1,2,3,4,5];
+// console.log(arr);
+// for(let i=0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// let item = [250, 645, 300,900, 50];
+//  for(let val of item){
+
+//    let i = 0;
+//     offer = val / 10;
+//     val = val-offer;
+//     console.log("offered value = "+ val);
+//     i++;
+//  }
+ 
+//  let item = [250, 645, 300,900, 50];
+
+//      for(let i=0; i<item.length; i++){
+//       let offer = item[i]/10;
+//       item[i] = item[i]-offer;
+      
+//      }
+//      console.log("offered val = "+ item);
+
+// let fooditems = ["Tomato", "mango", "apple", "litchi"];
+// console.log(fooditems)
+// fooditems.push("banana");
+// console.log(fooditems);
+// fooditems.pop();
+// console.log(fooditems.toString());
+// fooditems.unshift("chips");
+// console.log(fooditems);
+// fooditems.shift();
+// console.log(fooditems);
+
+let arr = [1,2,3,4,5,6,7,8,9];
+arr.splice(1,2);
+console.log(arr);
